2ebd7c01fc3efb19a09226dd38989336938298cd
luisito110 <99266934+luisito110@users.noreply.github.com>
2022-02-24 20:59:27 +0100

    Add files via upload

---
+++ funcion disparar.py
@@ -0,0 +1,13 @@
+def disparar_barco(fila, columna, tablero):
+    if agua(fila, columna, tablero):
+        tablero[fila][columna] = mal_disparo
+        print("Comandamante has fallado,mal disparo!")
+        return false
+
+    elif tablero[fila][columna]=mal_disparo or tablero[fila][columna]=buen_disparo
+    return false
+
+else:
+disparar_barco[fila][columna] = buen_disparo
+print("Comandante has acertado,buen disparo!")
+return true
