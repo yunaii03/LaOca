a0de5fc3845e80b56b9cce8c5d96f80035218fa4
Alejandro Gomez <98540437+agomez2106@users.noreply.github.com>
2022-02-24 20:40:42 +0100

    Add files via upload
    PEDIR COORDENADAS BATALLA NAVAL

---
+++ funcion_pedircoordenadas.py
@@ -0,0 +1,20 @@
+def pedir_coordenadas(jugador):
+    print ("Comandante, coordenadas exactas para colocar el Destructor (2 celdas): ")
+    x = int(input("Dime la fila: "))
+    y = int(input("Dime la columna: "))
+
+    print ("Comandante, coordenadas exactas para colocar el primer Submarino (3 celdas): ")
+    x1 = int(input("Dime la fila: "))
+    x2 = int(input("Dime la columna: "))
+
+    print ("Comandante, coordenadas exactas para colocar el segundo Submarino (3 celdas): ")
+    x1_1 = int(input("Dime la fila: "))
+    x2_2 = int(input("Dime la columna: "))
+
+    print ("Comandante, coordenadas exactas para colocar la Fragata (4 celdas): ")
+    x3 = int(input("Dime la fila: "))
+    x4 = int(input("Dime la columna: "))
+
+    print("¡¡¡Comandante, Portaviones operativo!!!, coordenadas exactas para colocarlo  (4 celdas): ")
+    x5 = int(input("Dime la fila: "))
+    x6 = int(input("Dime la columna: "))
